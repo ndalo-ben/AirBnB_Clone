346ef6f1ad6553f86e04b940c44dae3077265e07
ben <ndaloben8@gmail.com>
2024-11-04 20:47:00 +0300

    create sign up modal hook

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -10,19 +10,17 @@ const LoginModal = () => {
 
     const content = (
         <>
-            <h2 className="mb-6 text-2xl">Welcome to AirBnB, please log in</h2>
-
             <form className="space-y-4">
                 <input type="text" placeholder="email address" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
                 <input type="password" placeholder="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-                
+
                 <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
                     the error message
                 </div>
-                
+
                 <CustomButton
-                label="Submit"
-                onClick={() => console.log('test')}
+                    label="Submit"
+                    onClick={() => console.log('test')}
                 />
             </form>
         </>
