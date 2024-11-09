ff6b5708682787f8b4b405296c8769e2768806db
ben <ndaloben8@gmail.com>
2024-11-09 21:23:39 +0300

    implement user model

--- backend/bnbclone_backend/entrypoint.sh
+++ backend/bnbclone_backend/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if ["$DATABASE" = "postgres"] 
-  then 
-  echo "Check if database is running..."
+if [ "$DATABASE" = "postgres" ] 
+then 
+    echo "Check if database is running..."
 
-  while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-  done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
 
-  echo "The database is up and running :-D"
+    echo "The database is up and running :-D"
 
 fi
 
